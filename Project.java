52b1d34888a3ac7d9bd0806bf413125b92e412b9
Vincent <XxRandomdudexX12@gmail.com>
2025-08-14 21:40:18 +0800

    final ver

--- Project.java
+++ Project.java
@@ -3,12 +3,11 @@ import java.util.Scanner;
 public class Project {
     public static void main(String[] args) {
         Scanner scan = new Scanner (System.in);
-        int sqnum;
 
         System.out.println("Input number to be squared");
         int number = scan.nextInt();
         
-        sqnum = number*=number;
+        int sqnum = number*number;
 
         System.out.println(sqnum);
 
